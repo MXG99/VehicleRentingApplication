ef381e418f3d51160ca09c188980325a1e358a34
MXG99 <george99.bl@gmail.com>
2022-07-20 21:27:32 +0300

    Initial commit

---
+++ src/test/java/com/siemens/VehicleRentingApplication/VehicleRentingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siemens.VehicleRentingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
